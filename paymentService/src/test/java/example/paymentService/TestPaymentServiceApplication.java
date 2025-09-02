34cd42e126a7b3a95ff11effae5c11d6f5a50289
alex188w <alex18w@mail.ru>
2025-09-02 22:27:00 +0500

    multiProject_structure

---
+++ paymentService/src/test/java/example/paymentService/TestPaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package example.paymentService;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PaymentServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
