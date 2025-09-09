016dd99050d518f0212ff37a886fec88b33d0d93
alex188w <alex18w@mail.ru>
2025-09-09 23:38:35 +0500

    readme

--- openapiContract/target/generated-sources/client/src/main/java/com/example/openapi/client/api/PaymentApi.java
+++ openapiContract/target/generated-sources/client/src/main/java/com/example/openapi/client/api/PaymentApi.java
@@ -7,7 +7,9 @@ import com.example.openapi.client.model.PaymentResponse;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.LinkedMultiValueMap;
@@ -15,12 +17,14 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.12.0")
 public class PaymentApi {
